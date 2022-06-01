2f66aaeb3bfa26972059b0a9f5f0dec976571347
Victor Wei <vw1262@gmail.com>
2022-06-01 10:17:47 -0500

    add ui tests

--- lints/invalid_account_data/ui/secure/src/lib.rs
+++ lints/invalid_account_data/ui/secure/src/lib.rs
@@ -8,12 +8,15 @@ use spl_token::state::Account as SplTokenAccount;
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-pub mod account_data_matching_insecure {
+pub mod account_data_matching_secure {
     use super::*;
 
     pub fn log_message(ctx: Context<LogMessage>) -> ProgramResult {
         let token = SplTokenAccount::unpack(&ctx.accounts.token.data.borrow())?;
-        msg!("Your account balance is: {}", token.amount);
+        if ctx.accounts.authority.key != &token.owner {
+            return Err(ProgramError::InvalidAccountData);
+        }
+        msg!("Your acocunt balance is: {}", token.amount);
         Ok(())
     }
 }
