88cd15ae1a567a6f7a1733e74c2643d7c871d20f
Victor Wei <vw1262@gmail.com>
2022-06-15 14:24:53 -0500

    basic impl of logic of type-cosplay

--- crate/src/paths.rs
+++ crate/src/paths.rs
@@ -3,3 +3,4 @@ pub const ANCHOR_LANG_SIGNER: [&str; 4] = ["anchor_lang", "accounts", "signer", 
 
 pub const SOLANA_PROGRAM_ACCOUNT_INFO: [&str; 3] =
     ["solana_program", "account_info", "AccountInfo"];
+pub const ACCOUNT_DISCRIMINANT: [&str; 1] = ["AccountDiscriminant"];
--- lints/type_cosplay/src/lib.rs
+++ lints/type_cosplay/src/lib.rs
@@ -1,26 +1,15 @@
 #![feature(rustc_private)]
 #![warn(unused_extern_crates)]
 
-extern crate rustc_ast;
-extern crate rustc_ast_pretty;
-extern crate rustc_data_structures;
-extern crate rustc_errors;
 extern crate rustc_hir;
-extern crate rustc_hir_pretty;
-extern crate rustc_index;
-extern crate rustc_infer;
-extern crate rustc_lexer;
-extern crate rustc_middle;
-extern crate rustc_mir_dataflow;
-extern crate rustc_parse;
-extern crate rustc_parse_format;
 extern crate rustc_span;
-extern crate rustc_target;
-extern crate rustc_trait_selection;
-extern crate rustc_typeck;
 
+use solana_lints::{paths};
+use clippy_utils::{diagnostics::span_lint, ty::match_type, SpanlessEq};
 use rustc_lint::{LateContext, LateLintPass};
-use rustc_hir::{Item, VariantData};
+use rustc_hir::{Item, ItemKind, def::Res, Path, QPath, TyKind, HirId, Mod, FieldDef, Ty};
+use rustc_span::Span;
+use if_chain::if_chain;
 
 dylint_linting::declare_late_lint! {
     /// **What it does:**
@@ -44,25 +33,77 @@ dylint_linting::declare_late_lint! {
 }
 
 impl<'tcx> LateLintPass<'tcx> for TypeCosplay {
-    // if there are 2 structs that have the same fields, then flag lint
-    // CONSIDERATIONS:
-    // If it is semantically meant for 2 types to have the same fields, then adding
-    // an arbitrary "dummy" field is weird. It makes more sense to add a "discriminant"
-    // field, as is suggested. But then there needs to also be a check on this discriminant
-    // field, as in the secure example. Should we enforce this check? Or is that another lint's job?
+    fn check_mod(
+        &mut self,
+        cx: &LateContext<'tcx>,
+        module: &'tcx Mod<'tcx>,
+        span: Span,
+        _: HirId
+    ) {
+        if !span.from_expansion() {
+            // this code could be replaced with visit_item?
+            // collect only struct items
+            let struct_field_ty_reprs: StructFieldTypeReprArray = StructFieldTypeReprArray::new();
+            module.item_ids.iter().for_each(|id| {
+                let item = cx.tcx.hir().item(*id);
 
-    // or post?
-    // fn check_struct_def_post(&mut self, cx: &LateContext<'tcx>, variant_data: &'tcx VariantData<'tcx>) {
-    //     println!("{:?}", variant_data);
-    // }
+                // filter out only struct items and convert to field_ty_repr
+                if let ItemKind::Struct(variant_data, _) = &item.kind {
+                    let struct_field_ty_repr = convert_struct_to_field_tys(variant_data.fields());
+                    // println!("{:#?}", struct_field_ty_repr);
 
-    fn check_item(&mut self, cx: &LateContext<'tcx>, item: &'tcx Item<'tcx>) {
-        // collect struct definitions
-        //  while collecting, if encountered an equivalent struct def, flag lint
-        if let ItemKind::Struct(_, _) = item.kind {
-            println!("{:?}", item);
+                    // if the array has a matching struct (ie, all fields are the same, in order, and no
+                    // AccountDiscriminant field)
+                    if struct_field_ty_reprs.has_match(cx, struct_field_ty_repr) {
+                        span_lint(
+                            cx,
+                            TYPE_COSPLAY,
+                            item.span,
+                            "TODO"
+                        )
+                    } else {
+                        struct_field_ty_reprs.0.push(struct_field_ty_repr);
+                    }
+                }
+            });
         }
     }
+}
+
+/// A vector of StructFieldTypeRepr
+struct StructFieldTypeReprArray<'hir>(Vec<StructFieldTypeRepr<'hir>>);
+
+// TODO: probably better way to format data structures
+impl<'hir> StructFieldTypeReprArray<'hir> {
+    pub fn new() -> Self {
+        StructFieldTypeReprArray(vec![])
+    }
+
+    // returns true if all fields are the same type and same order
+    pub fn has_match(&self, cx: &LateContext, other: StructFieldTypeRepr) -> bool {
+        if self.0.len() == 0 {
+            false
+        } else {
+            // TODO: turn into iterator
+            let mut equals = false;
+            for item in self.0 {
+                equals = item.0.iter().zip(other.0.iter())
+                    .all(|(x, y)| eq_ty(x, y) && !match_type(cx, x, &paths::ACCOUNT_DISCRIMINANT));
+                
+                if equals {
+                    return equals;
+                }
+            }
+            equals
+        }
+    }
+}
+
+/// A representation of a struct as a vector of its field types
+struct StructFieldTypeRepr<'hir>(Vec<&'hir Ty<'hir>>);
+
+fn convert_struct_to_field_tys<'hir>(field_defs: &'hir [FieldDef<'hir>]) -> StructFieldTypeRepr {
+    StructFieldTypeRepr(field_defs.iter().map(|def| def.ty).collect())
 }
 
 #[test]
@@ -80,3 +121,35 @@ fn secure() {
     dylint_testing::ui_test_example(env!("CARGO_PKG_NAME"), "secure");
 }
 
+// fn check_item(&mut self, cx: &LateContext<'tcx>, item: &'tcx Item<'tcx>) {
+//     // collect struct definitions
+//     //  while collecting, if encountered an equivalent struct def, flag lint
+//     // with the exception of if one of the field types == AccountDiscriminant
+//     let structs = vec![];
+
+//     if_chain! {
+//         if !item.span.from_expansion();
+//         if let ItemKind::Struct(variant_data, _) = &item.kind;
+//         let field_def_ids = variant_data.fields().iter().for_each(|field| {
+//             if_chain! {
+//                 if let TyKind::Path(qpath) = field.ty.kind;
+//                 if let QPath::Resolved(_, path) = qpath;
+//                 if let Res::Def(_, def_id) = path.res;
+//                 then {
+//                     def_id
+//                 } else {
+//                     return;
+//                 }
+//             }
+//         }).collect();
+//         let _ = println!("{:#?}", variant_data.fields());
+
+//         // if there exists another struct such that they match, then flag lint
+//         if structs.has_match(field_def_ids);
+//         then {
+//             // let _ = println!("{:#?}", item);
+            
+//         }
+        
+//     }
+// }
