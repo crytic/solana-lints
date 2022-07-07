01d504483d586b22e3118c4b0af4b6fc9a2f5f0b
Victor Wei <vw1262@gmail.com>
2022-07-06 20:43:58 -0500

    reimplementation of type-cosplay using check_expr and check_crate_post

--- lints/type_cosplay/src/lib.rs
+++ lints/type_cosplay/src/lib.rs
@@ -7,19 +7,21 @@ extern crate rustc_index;
 extern crate rustc_middle;
 extern crate rustc_span;
 
+use std::collections::HashMap;
+
 use clippy_utils::{diagnostics::span_lint_and_help, match_def_path, ty::match_type};
-use rustc_hir::{def::Res, Expr, ExprKind, QPath, TyKind, Mod, HirId};
+use rustc_hir::{def::Res, Expr, ExprKind, QPath, TyKind, Mod, HirId, intravisit::{walk_item, walk_expr}};
 use rustc_hir::*;
-use rustc_span::Span;
+use rustc_span::{Span, def_id::DefId};
 use rustc_index::vec::Idx;
 use rustc_lint::{LateContext, LateLintPass};
-use rustc_middle::ty::{AdtDef, TyKind as MiddleTyKind};
+use rustc_middle::ty::{AdtDef, TyKind as MiddleTyKind, Ty as MiddleTy, AdtKind};
 use solana_lints::{paths, utils::visit_expr_no_bodies};
 use rustc_hir::intravisit::Visitor;
 
 use if_chain::if_chain;
 
-dylint_linting::declare_late_lint! {
+dylint_linting::impl_late_lint! {
     /// **What it does:**
     ///
     /// **Why is this bad?**
@@ -37,109 +39,91 @@ dylint_linting::declare_late_lint! {
     /// ```
     pub TYPE_COSPLAY,
     Warn,
-    "type is equivalent to another type"
+    "type is equivalent to another type",
+    TypeCosplay::default()
 }
 
-// TODO:
-// fix global state issue -- implement check mod
-// change logic to use containers
+#[derive(Default)]
+struct TypeCosplay {
+    deser_types: HashMap<AdtKind, Vec<(DefId, Span)>>,
+}
 
-impl<'tcx> LateLintPass<'tcx> for TypeCosplay {
-    // check mod can check the entire program. if we are writing this lint for only anchor programs,
-    // we can filter to find the main #program mod. I think there is only 1 mod, but not sure.
-    // fn check_mod(
-    //     &mut self,
-    //     cx: &LateContext<'tcx>,
-    //     module: &'tcx Mod<'tcx>,
-    //     span: Span,
-    //     hir_id: HirId
-    // ) {
-    //     let mut enum_type: Option<MiddleTyKind> = None;
-    //     let mut num_struct_types = 0;
-
-    //     if !span.from_expansion() {
-    //         module.item_ids.iter().for_each(|id| {
-    //             let item = cx.tcx.hir().item(*id);
-
-    //         })
-    //         println!("{:#?}", module);
-    //     }
-    // }
-
-    fn check_body(&mut self, cx: &LateContext<'tcx>, body: &'tcx Body<'tcx>) {
-        let mut enum_type: Option<MiddleTyKind> = None;
-        let mut num_struct_types = 0;
-
-        let visitor = V {
-            cx,
-        };
-
-        visitor.visit_expr(body.value);
-        // println!("{:#?}", body.value.span);
-        
+// Returns true if `map` contains a single key-value pair, and the value contains
+// only a single element.
+fn contains_single_deserialized_type(map: &HashMap<AdtKind, Vec<(DefId, Span)>>) -> Option<(DefId, Span)> {
+    match map.len() {
+        1 => {
+            // if there is only 1 k-v pair, then there will only be a single value
+            let value = map.values().next().unwrap();
+            match value.len() {
+                1 => Some(value[0]),
+                _ => None,
+            }
+        },
+        _ => None,
     }
 }
 
-struct V<'cx> {
-    cx: &'cx LateContext<'cx>
-}
-
-impl<'cx, 'tcx> Visitor<'tcx> for V<'cx> {
-    fn visit_expr(&mut self, expr: &'tcx Expr<'tcx>) {
-        // all the logic previously in check_expr()
+impl<'tcx> LateLintPass<'tcx> for TypeCosplay {
+    fn check_expr(&mut self, cx: &LateContext<'tcx>, expr: &'tcx Expr<'tcx>) {
         if_chain! {
             if !expr.span.from_expansion();
             if let ExprKind::Call(fnc_expr, args_exprs) = expr.kind;
-            if is_deserialize_function(self.cx, fnc_expr);
+            if is_deserialize_function(cx, fnc_expr);
             // walk each argument expression and see if the data field is referenced
             if args_exprs
                 .iter()
-                .any(|arg| visit_expr_no_bodies(arg, |expr| contains_data_field_reference(self.cx, expr)));
-            // Checking the type that the deser function was called on is an enum
+                .any(|arg| visit_expr_no_bodies(arg, |expr| contains_data_field_reference(cx, expr)));
+            // get the type that the function was called on, ie X in X::deser()
             if let ExprKind::Path(qpath) = &fnc_expr.kind;
             if let QPath::TypeRelative(ty, _) = qpath;
             if let TyKind::Path(ty_qpath) = &ty.kind;
-            let res = self.cx.typeck_results().qpath_res(ty_qpath, ty.hir_id);
+            let res = cx.typeck_results().qpath_res(ty_qpath, ty.hir_id);
             if let Res::Def(_, def_id) = res;
             then {
-                let middle_ty = self.cx.tcx.type_of(def_id);
-                if middle_ty.is_enum() {
-                    enum_type = match enum_type {
-                        Some(t) => {
-                            if t != middle_ty {
-                                // TODO: check spans are correct
-                                span_lint_and_help(
-                                    self.cx,
-                                    TYPE_COSPLAY,
-                                    expr.span,
-                                    "warning: multiple enum types detected. Should only have 1 enum type to avoid possible equivalent types",
-                                    Some(ty.span),
-                                    "help: consider constructing a single enum that contains all type definitions as variants"
-                                )
-                            }
-                            Some(t)
-                        }
-                        None => Some(middle_ty)
-                    }
-                } else {
-                    if let MiddleTyKind::Adt(adt_def, _) = middle_ty.kind() {
-                        num_struct_types += 1;
-                        if !has_discriminant(self.cx, &adt_def, num_struct_types) {
-                            // TODO: check spans are correct
-                            span_lint_and_help(
-                                self.cx,
-                                TYPE_COSPLAY,
-                                ty.span,
-                                "warning: type definition does not have a proper discriminant. Types may be indistinguishable when deserialized",
-                                None,
-                                "help: add an enum with at least as many variants as there are struct definitions"
-                            )
-                        }
+                // insert type into hashmap
+                let middle_ty = cx.tcx.type_of(def_id);
+                if let MiddleTyKind::Adt(adt_def, _) = middle_ty.kind() {
+                    let adt_kind = adt_def.adt_kind();
+                    if let Some(vec) = self.deser_types.get_mut(&adt_kind) {
+                        vec.push((adt_def.did(), ty.span));
+                    } else {
+                        self.deser_types.insert(adt_kind, vec![(adt_def.did(), ty.span)]);
                     }
                 }
             }
         }
-        self.walk_expr(expr);
+    }
+
+    fn check_crate_post(&mut self, cx: &LateContext<'tcx>) {
+        // if the map contains a single deserialized type
+        if let Some((def_id, _)) = contains_single_deserialized_type(&self.deser_types) {
+            let adt_def = cx.tcx.adt_def(def_id);
+            if !adt_def.is_enum() {
+                check_structs_have_discriminant(cx, &self.deser_types);
+            }
+        } else {
+            // check if an enum type was deserialized
+            if let Some(enums) = self.deser_types.get(&AdtKind::Enum) {
+                if enums.len() == 1 {
+                    check_structs_have_discriminant(cx, &self.deser_types);
+                } else {
+                    let first_span = enums[0].1;
+                    let second_span = enums[1].1;
+                    span_lint_and_help(
+                        cx,
+                        TYPE_COSPLAY,
+                        first_span,
+                        "warning: multiple enum types deserialized. Should only have one enum type to avoid possible equivalent types",
+                        Some(second_span),
+                        "help: consider constructing a single enum that contains all type definitions as variants"
+                    )
+                }
+            } else {
+                // no deserialized enum, but multiple deserialized structs. Check each struct for a discriminant
+                check_structs_have_discriminant(cx, &self.deser_types);
+            }
+        }
     }
 }
 
@@ -149,9 +133,9 @@ fn is_deserialize_function(cx: &LateContext<'_>, expr: &Expr<'_>) -> bool {
         // for now, only testing borsh deserialize function
         if match_def_path(cx, def_id, &paths::BORSH_TRY_FROM_SLICE);
         then {
-            true
+            return true;
         } else {
-            false
+            return false;
         }
     }
 }
@@ -170,11 +154,18 @@ fn contains_data_field_reference(cx: &LateContext<'_>, expr: &Expr<'_>) -> bool 
     }
 }
 
-fn has_discriminant(cx: &LateContext, adt: &AdtDef, num_struct_types: usize) -> bool {
-    // check if it is an enum with #variants >= #equivalent struct types in code
-    let variant = adt.variants().get(Idx::new(0)).unwrap();
+// TODO: could be a method
+fn check_structs_have_discriminant(cx: &LateContext<'_>, deser_types: &HashMap<AdtKind, Vec<(DefId, Span)>>) {
+    if let Some(types) = deser_types.get(&AdtKind::Struct) {
+        let num_structs = types.len();
+        types.iter().for_each(|t| has_discriminant(cx, &cx.tcx.adt_def(t.0), num_structs, t.1));
+    }
+}
 
-    variant.fields.iter().any(|field| {
+/// Returns true if the `adt` has a field that is an enum and the number of variants of that enum is at least the number of deserialized struct types collected.
+fn has_discriminant(cx: &LateContext, adt: &AdtDef, num_struct_types: usize, span: Span) {
+    let variant = adt.variants().get(Idx::new(0)).unwrap();
+    let has_discriminant = variant.fields.iter().any(|field| {
         let ty = cx.tcx.type_of(field.did);
         if_chain! {
             if let MiddleTyKind::Adt(adt_def, _) = ty.kind();
@@ -186,7 +177,18 @@ fn has_discriminant(cx: &LateContext, adt: &AdtDef, num_struct_types: usize) -> 
                 false
             }
         }
-    })
+    });
+
+    if !has_discriminant {
+        span_lint_and_help(
+            cx,
+            TYPE_COSPLAY,
+            span,
+            "warning: type does not have a proper discriminant. It may be indistinguishable when deserialized",
+            None,
+            "help: add an enum with at least as many variants as there are struct definitions"
+        )
+    }
 }
 
 #[test]
