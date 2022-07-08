a477cfbdb9a07d54bcb071ea4790fd4b14e927f3
Samuel Moelius <samuel.moelius@trailofbits.com>
2022-07-08 19:02:36 +0000

    Add `lints` test

---
+++ crate/tests/lints.rs
@@ -0,0 +1,27 @@
+use assert_cmd::prelude::*;
+use std::{
+    fs::read_dir,
+    io::{stderr, Write},
+    path::Path,
+};
+
+#[test]
+fn lints() {
+    let dir = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .join("..")
+        .join("lints");
+
+    for entry in read_dir(dir).unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path();
+
+        writeln!(stderr(), "{:?}", path.canonicalize().unwrap()).unwrap();
+
+        std::process::Command::new("cargo")
+            .current_dir(path)
+            .env_remove("RUSTUP_TOOLCHAIN")
+            .args(["test"])
+            .assert()
+            .success();
+    }
+}
