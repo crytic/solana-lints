10db3acd8332cf7f0589feaa77e1b1ca868730c2
Victor Wei <vw1262@gmail.com>
2022-08-10 08:35:35 -0500

    add anchor check to type-cosplay

--- crate/src/paths.rs
+++ crate/src/paths.rs
@@ -4,3 +4,4 @@ pub const ANCHOR_LANG_SIGNER: [&str; 4] = ["anchor_lang", "accounts", "signer", 
 pub const SOLANA_PROGRAM_ACCOUNT_INFO: [&str; 3] =
     ["solana_program", "account_info", "AccountInfo"];
 pub const BORSH_TRY_FROM_SLICE: [&str; 4] = ["borsh", "de", "BorshDeserialize", "try_from_slice"];
+
