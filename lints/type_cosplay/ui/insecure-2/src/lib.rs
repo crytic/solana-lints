0e7263e87c025562a4674eb4982f3551f46cf3e2
Victor Wei <vw1262@gmail.com>
2022-08-11 14:29:02 -0500

    add descriptions for all type-cosplay examples

--- lints/type_cosplay/ui/insecure-2/src/lib.rs
+++ lints/type_cosplay/ui/insecure-2/src/lib.rs
@@ -3,16 +3,6 @@ use borsh::{BorshDeserialize, BorshSerialize};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
-// NOTE: this is insecure because the program tries to deserialize from multiple enum types.
-// This is a vulnerability because it partitions the "set of types" and is thus not exhaustive
-// in discriminating between all types. Deserializing from a single enum is safe because the
-// program will have to match the deserialized result against an enum variant in order to reconstruct
-// the ADT. This effectively serves as a discriminant, because even if two types deserialize the same,
-// the programmer will explicitly match it to a type.
-// If multiple enums are used to encompass the types, this defense will basically be breached.
-// There may be two different types that are equivalent. If we deserialize from an enum,
-// type B could be passed in, when what was expected was type A.
-
 // NOTE: what about the case where we only deserialize from one enum? Could this still be a vulnerability?
 // ex. only deser from UserInfo. Someone sets data as MetadataInfo::Metadata(...). will this match?
 // what if MetadataInfo::User(...)?
