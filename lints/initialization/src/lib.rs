de23eef50dc0d64d0238b6d3555cc2efbb28ead1
Victor Wei <vw1262@gmail.com>
2022-08-15 20:36:10 -0500

    initialization lint notes

---
+++ lints/initialization/src/lib.rs
@@ -0,0 +1,60 @@
+#![feature(rustc_private)]
+#![warn(unused_extern_crates)]
+
+extern crate rustc_ast;
+extern crate rustc_ast_pretty;
+extern crate rustc_data_structures;
+extern crate rustc_errors;
+extern crate rustc_hir;
+extern crate rustc_hir_pretty;
+extern crate rustc_index;
+extern crate rustc_infer;
+extern crate rustc_lexer;
+extern crate rustc_middle;
+extern crate rustc_mir_dataflow;
+extern crate rustc_parse;
+extern crate rustc_parse_format;
+extern crate rustc_span;
+extern crate rustc_target;
+extern crate rustc_trait_selection;
+extern crate rustc_typeck;
+
+use rustc_lint::LateLintPass;
+
+dylint_linting::declare_late_lint! {
+    /// **What it does:**
+    ///
+    /// **Why is this bad?**
+    ///
+    /// **Known problems:** None.
+    ///
+    /// **Example:**
+    ///
+    /// ```rust
+    /// // example code where a warning is issued
+    /// ```
+    /// Use instead:
+    /// ```rust
+    /// // example code that does not raise a warning
+    /// ```
+    pub INITIALIZATION,
+    Warn,
+    "description goes here"
+}
+
+impl<'tcx> LateLintPass<'tcx> for Initialization {}
+
+#[test]
+fn insecure() {
+    dylint_testing::ui_test_example(env!("CARGO_PKG_NAME"), "insecure");
+}
+
+#[test]
+fn recommended() {
+    dylint_testing::ui_test_example(env!("CARGO_PKG_NAME"), "recommended");
+}
+
+#[test]
+fn secure() {
+    dylint_testing::ui_test_example(env!("CARGO_PKG_NAME"), "secure");
+}
