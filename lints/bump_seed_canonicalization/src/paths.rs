a999952f49df3b93e040c5cb60dd841c51f9611b
Samuel Moelius <samuel.moelius@trailofbits.com>
2022-08-20 16:09:05 +0000

    Use top-level `paths` module

--- lints/bump_seed_canonicalization/src/paths.rs
+++
@@ -1,8 +0,0 @@
-pub const CREATE_PROGRAM_ADDRESS: [&str; 4] = [
-    "solana_program",
-    "pubkey",
-    "Pubkey",
-    "create_program_address",
-];
-
-pub const ACCOUNT_DESERIALIZE: [&str; 2] = ["anchor_lang", "AccountDeserialize"];
