c1f5d5d146b1d91e9f56f7c33ae239b397a52e5b
Samuel Moelius <samuel.moelius@trailofbits.com>
2022-10-29 00:02:41 +0000

    Add `ui_tests_use_distinct_package_names` test

--- crate/tests/meta.rs
+++ crate/tests/meta.rs
@@ -1,3 +1,9 @@
+#![feature(rustc_private)]
+#![warn(unused_extern_crates)]
+
+extern crate rustc_data_structures;
+
+use rustc_data_structures::fx::FxHashMap;
 use std::{
     ffi::OsStr,
     fs::{read_dir, read_to_string},
@@ -46,6 +52,43 @@ fn only_lints_have_lockfiles() {
         let path = Path::new(line);
         if path.file_name() == Some(OsStr::new("Cargo.lock")) {
             assert_eq!(path.parent(), Some(Path::new("lints")));
+        }
+    }
+}
+
+#[test]
+fn ui_tests_use_distinct_package_names() {
+    let mut name_path_map = FxHashMap::default();
+    for entry in read_dir("../lints")
+        .unwrap()
+        .flat_map(|entry| read_dir(entry.unwrap().path().join("ui")).unwrap())
+    {
+        let entry = entry.unwrap();
+        let path_curr = entry.path().canonicalize().unwrap();
+
+        let toml = read_to_string(path_curr.join("Cargo.toml"))
+            .unwrap()
+            .parse::<toml::Value>()
+            .unwrap();
+
+        let package = toml
+            .as_table()
+            .and_then(|table| table.get("package"))
+            .and_then(Value::as_table)
+            .unwrap();
+
+        let name = package
+            .get("name")
+            .and_then(Value::as_str)
+            .unwrap()
+            .replace('-', "_");
+
+        if let Some(path_prev) = name_path_map.insert(name.clone(), path_curr.clone()) {
+            panic!(
+                "duplicate package name: {name:?}
+ first package: {path_prev:?}
+second package: {path_curr:?}"
+            );
         }
     }
 }
